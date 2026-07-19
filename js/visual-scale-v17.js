9bef54bddb1ed60c1e17de82dcf8dcb072917ecf
DreddyCZE <DreddyCZE@gmail.com>
2026-07-19 14:20:34 +0200

    Increase collectible and character visual sizes

---
+++ js/visual-scale-v17.js
@@ -0,0 +1,24 @@
+(() => {
+  const factors = {
+    lili: 1.35,
+    star: 1.85,
+    key: 1.75,
+    pond: 1.45,
+    gate: 1.75
+  };
+
+  const originalSetDisplaySize = Phaser.GameObjects.Components.Size.prototype.setDisplaySize;
+
+  Phaser.GameObjects.Components.Size.prototype.setDisplaySize = function (width, height) {
+    const key = this.texture?.key;
+    const factor = factors[key] ?? 1;
+    const result = originalSetDisplaySize.call(this, width * factor, height * factor);
+
+    if (factor > 1) {
+      this.setDataEnabled?.();
+      this.setData?.('gardenVisualScale', factor);
+    }
+
+    return result;
+  };
+})();
