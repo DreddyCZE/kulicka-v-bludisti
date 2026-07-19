897c2d557daf0b547d742c8589c5a452bc91e63e
DreddyCZE <DreddyCZE@gmail.com>
2026-07-19 13:47:21 +0200

    Refresh cache for control mode v14

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kouzelna-zahrada-v13';
-const ASSETS=['./','./index.html','./phaser.html','./manifest.json','./css/game.css','./css/phaser.css?v=13','./js/main.js','./js/data.js','./js/storage.js','./js/art.js','./js/phaser-game-v10.js?v=13'];
+const CACHE='kouzelna-zahrada-v14';
+const ASSETS=['./','./index.html','./phaser.html','./manifest.json','./css/game.css','./css/phaser.css?v=14','./js/main.js','./js/data.js','./js/storage.js','./js/art.js','./js/control-mode-v14.js?v=14','./js/phaser-game-v10.js?v=14'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())});
 self.addEventListener('fetch',event=>{const request=event.request;if(request.method!=='GET')return;const url=new URL(request.url);if(url.origin===self.location.origin){event.respondWith(fetch(request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(request,copy));return response}).catch(()=>caches.match(request).then(response=>response||caches.match('./index.html'))));return}event.respondWith(caches.match(request).then(response=>response||fetch(request)))})
