f047282d49fda7aacf07b318c3272f65910903e6
DreddyCZE <DreddyCZE@gmail.com>
2026-07-19 12:45:16 +0200

    Refresh cache for responsive Phaser layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kouzelna-zahrada-v6';
+const CACHE='kouzelna-zahrada-v7';
 const ASSETS=['./','./index.html','./phaser.html','./manifest.json','./css/game.css','./css/phaser.css','./js/main.js','./js/data.js','./js/storage.js','./js/art.js','./js/phaser-game.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
